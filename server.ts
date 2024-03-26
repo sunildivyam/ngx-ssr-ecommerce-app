f3854524c9432700add92041050b77ff5918f1d0
Sunil Kumar <sunil.divyam@gmail.com>
2024-03-26 20:55:34 +0530

    Initial Setup

---
+++ server.ts
@@ -0,0 +1,116 @@
+import 'zone.js/node';
+
+import { APP_BASE_HREF } from '@angular/common';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import * as express from 'express';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+import * as cors from 'cors';
+import * as bodyparser from 'body-parser';
+import { AppServerModule } from './src/main.server';
+import { environment } from './src/environments/environment';
+import { env } from 'node:process';
+import { imagesRouter } from '@annuadvent/ngx-tools/fire-apis';
+
+// APIs Routers
+// import { usersRouter } from '@annuadvent/ngx-tools/fire-apis';
+
+const setNodeEnv = (): void => {
+  // Set Node Environment Variable
+  env.NODE_ENV = environment.development
+    ? 'development'
+    : environment.staging
+    ? 'staging'
+    : environment.development
+    ? 'production'
+    : '';
+
+  // Set Firebase Config
+  env.FIREBASE_CONFIG = JSON.stringify(environment.appConfig.firebase.app);
+};
+
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
+  // Set NODE envs
+  setNodeEnv();
+
+  // source paths for index.html
+  const distFolder = join(process.cwd(), 'dist/ngx-ssr-app/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? 'index.original.html'
+    : 'index';
+
+  // Server side settings
+  const server = express();
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
+  server.use(cors());
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
+  server.use(bodyparser.json());
+
+  // REST Express API endpoints
+  // Add custom APIs | APP_SPECIFIC
+
+  server.use('/api/images', imagesRouter);
+
+  // All other
+  server.use(['/api', '/api/*'], (req, res) =>
+    res.status(503).send('API url does not exist.')
+  );
+
+  // Serve static files from /browser
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    })
+  );
+
+  // ANGULAR ROUTES
+
+  // Routes that skips server side rendering | APP_SPECIFIC
+  server.get(['/login', '/dashboard', '/dashboard/**'], (req, res) => {
+    res.sendFile(join(distFolder, `${indexHtml}`));
+  });
+
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
+  server.engine(
+    'html',
+    ngExpressEngine({
+      bootstrap: AppServerModule,
+    })
+  );
+
+  // All regular routes use the Universal engine
+  server.get('*', (req, res) => {
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
+  });
+
+  return server;
+}
+
+function run(): void {
+  const port = process.env['PORT'] || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = (mainModule && mainModule.filename) || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
+export * from './src/main.server';
---
+++ src/app/components/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, NgZone } from "@angular/core";
+import { MenuItem } from "@annuadvent/ngx-common-ui/menu";
+import { SpinnerMode } from "@annuadvent/ngx-common-ui/spinner";
+import { ThemeService } from "@annuadvent/ngx-common-ui/theme";
+import { AppSpinnerService } from "../../modules/app-core/services/app-spinner.service";
+import { AppStateService } from "../../modules/app-core/services/app-state.service";
+import { AppState } from "../../modules/app-core/interfaces/app-state.interface";
+import { AppConfigService, AppConfig } from "@annuadvent/ngx-core/app-config";
+import {
+  SOCIAL_MEDIA_BUTTONS,
+  SocialMediaButton,
+} from "@annuadvent/ngx-common-ui/social-media";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent implements OnInit {
+  appConfig: AppConfig;
+  mainMenuItems: Array<MenuItem> = [];
+  footerNavItems: Array<MenuItem> = [];
+  isMainNavOpen: boolean = false;
+  SpinnerMode = SpinnerMode;
+  themeFontSizes: Array<string> = ["12px", "16px", "20px"];
+  socialMediaButtons: Array<SocialMediaButton> = [];
+  menuHeight: number = 0;
+
+  constructor(
+    private themeService: ThemeService,
+    public appSpinner: AppSpinnerService,
+    private appStateService: AppStateService,
+    private appConfigService: AppConfigService,
+    private zone: NgZone
+  ) {
+    this.appConfig = this.appConfigService.config;
+
+    this.appStateService.appState.subscribe((appState: AppState) => {
+      this.mainMenuItems = appState.mainNavItems as Array<MenuItem>;
+      this.footerNavItems = appState.mainNavItems as Array<MenuItem>;
+    });
+
+    // init social media
+    this.initSocialMedia();
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.themeService.setTheme(this.appConfig.themeName, true);
+  }
+
+  public loginStatusClicked(): void {
+    this.isMainNavOpen = !this.isMainNavOpen;
+  }
+
+  public mainMenuOpenStatusChanged(opened: boolean): void {
+    this.isMainNavOpen = opened;
+  }
+
+  public initSocialMedia(): void {
+    const socialMeidaConfig = this.appConfigService.config.socialMedia || {};
+
+    this.socialMediaButtons = SOCIAL_MEDIA_BUTTONS.map((btn) => {
+      return {
+        ...btn,
+        url: socialMeidaConfig[btn.id],
+      };
+    });
+  }
+
+  public onMainMenuResize(height: number): void {
+    this.zone.run(() => setTimeout(() => (this.menuHeight = height)));
+  }
+}
